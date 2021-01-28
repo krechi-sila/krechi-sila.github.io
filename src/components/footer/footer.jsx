2584dfcda43ef70a151707ab1ff7eba1e7cac54c
alexbaumgertner <alex.baumgertner@gmail.com>
2021-01-28 21:11:00 +0300

    Update copyright year

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,7 +6,7 @@ const Footer = (props) => {
   return (
     <footer className="footer">
       <span className="copyright footer__item">
-        2003 — 2017, <a href="//metrika.yandex.ru/stat/?id=414633">Кречевицы в сети</a>
+        2003 — 2021, <a href="//metrika.yandex.ru/stat/?id=414633">Кречевицы в сети</a>
       </span>
       <span className="feedback footer__item">
         Пишите: <a href="mailto:krechi-sila@yandex.ru">krechi-sila@yandex.ru</a>
