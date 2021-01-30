8cc7dea1050884abc823df13f5b8f7b3c2f043c4
alexbaumgertner <alex.baumgertner@gmail.com>
2021-01-30 23:47:01 +0300

    Add "Photos" page

---
+++ pages/photos/index.jsx
@@ -0,0 +1,43 @@
+import fs from 'fs'
+import matter from 'gray-matter'
+import Head from 'next/head'
+import path from 'path'
+import remark from 'remark'
+import html from 'remark-html'
+import unwrapImages from 'remark-unwrap-images'
+import { PostLayout } from '../../layouts'
+
+export async function getStaticProps () {
+  const dataDirPath = path.join(process.cwd(), 'posts')
+  const fileFullPath = path.join(dataDirPath, `photos.md`)
+  const fileContents = fs.readFileSync(fileFullPath, 'utf8')
+  const matterResult = matter(fileContents)
+  const processedContent = await remark()
+    .use(html)
+    .use(unwrapImages)
+    .process(matterResult.content)
+  const contentHtml = processedContent.toString()
+
+  return {
+    props: {
+      pageData: {
+        contentHtml,
+        ...matterResult.data,
+      },
+    },
+  }
+}
+
+export default function PhotosPage ({ pageData }) {
+  return (
+    <PostLayout>
+      <Head>
+        <title>{pageData.title}</title>
+      </Head>
+      <article>
+        <h1 className="page__header">{pageData.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: pageData.contentHtml }} />
+      </article>
+    </PostLayout>
+  )
+}
---
+++ posts/photos.md
@@ -0,0 +1,12 @@
+---
+title: 'Фотоальбомы'
+date: '2021-01-30'
+---
+
+- [Заметки Михаила Кривых о Кречевицах](https://mikhail.krivyy.com/tag/кречевицы/)
+- [110 ВОЕННО-ТРАНСПОРТНЫЙ АВИАЦИОННЫЙ ПОЛК](/https://110vtap.ucoz.ru/index/istorija_v_fotografijakh/0-14)
+- [Прощай полк прощай знамя полковое](https://photos.google.com/share/AF1QipMdbQ2PI3j6vhcA2EPEYRHE7C0mmMKzKIF2Owk9S00bOQOoE7TZlrMk4TC7aIs7tw?key=dVJYR0hHem1yVXNXOTVrdzJLZE1JREdkdS1xS05R)
+- [Кречевицы на Яндекс-Диске](https://yadi.sk/a/odkTbutr3XycL5)
+- [Кречевицы, 22.10.2007 — 7.08.2008](https://yadi.sk/a/6pcd-hDK3XycXJ)
+- [Кречевицы, 2008, снег](https://yadi.sk/a/A2jZc90X3XycaY)
+- Кречевицы, фотографии [Вконтакте-группы](https://vk.com/albums-239849)
