c526e0928a65e11eea628002aeff6b3165f11bed
alexbaumgertner <alex.baumgertner@gmail.com>
2022-11-02 10:40:01 +0300

    Update info: links to events

--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,35 @@
-import { useEffect } from 'react'
+import Link from 'next/link'
 import { Header } from 'semantic-ui-react'
 
 import { PagesLayout } from '../layouts'
 
 export default function Home () {
-
-  useEffect(() => {
-    const script = document.createElement('script')
-    script.src = '//vk.com/js/api/openapi.js?168'
-    script.onload = function () {
-      if (window.VK) {
-        VK.Widgets.Group(
-          'vk_groups',
-          {
-            mode: 2,
-            wide: 0,
-            width: '700',
-            height: '800',
-          },
-          239849,
-        )
-      }
-
-    }
-
-    document.body.appendChild(script)
-  }, [])
-
   return (
     <PagesLayout home>
       <section className="ui text container">
         <Header as="h1">О проекте</Header>
         <article className="ui container">
           <p>Кречеви́цы — микрорайон Великого Новгорода, до 2004 года — посёлок городского типа.</p>
+          <br/>
           <p>
-            В Кречевицах переплелась многовековая история России — от военных поселений Аракчеева и Драгунского полка до многих значимых событий
+            В Кречевицах переплелась многовековая история России — от
+            <a href="https://ru.wikipedia.org/wiki/Военные_поселения#Военные_поселения_в_Новгородской_губернии">военных поселений Аракчеева</a> и <a
+            href="https://ru.wikipedia.org/wiki/Драгуны#История_появления_драгун">Драгунского полка</a> до многих значимых событий
             20 века — 110 авиаполк выполнял задания
-            в Алжире, Египте, Сирии, Ираке, Афганистане, Анголе, первой/второй Чеченской войне, Южной Осетии.
+            в Алжире, Египте, Сирии, Ираке, <a href="https://ru.wikipedia.org/wiki/Афганская_война_(1979—1989)">Афганистане</a>, Анголе, <a
+            href="https://ru.wikipedia.org/wiki/Первая_чеченская_война">первой</a>/<a
+            href="https://ru.wikipedia.org/wiki/Вторая_чеченская_война">второй</a> Чеченской войне, <a
+            href="https://ru.wikipedia.org/wiki/Война_в_Грузии_(2008)">Южной Осетии</a>.
           </p>
+          <br/>
           <p>
-            В 2009 полк был расформирован, аэродром хотели сделать гражданским, много лет были проекты с привлечением инвесторов, но как-то не заладилось.
+            В <Link href="memories/2009-10-14-regiment-disbandment">2009 полк был расформирован</Link>,
+            много лет <Link href="/airfield">обсуждаются проекты переоборудования аэродрома</Link> под гражданские самолеты.
           </p>
+          <br/>
           <p>Кречевицах существует филиал <a href="http://www.pegas-center.ru/">авиаклуба «Пегас»</a> для малой авиации.</p>
         </article>
       </section>
-
-      <Header as="h3">Группа Вконтакте</Header>
-      <div className="vkVidget">
-        <div id="vk_groups" />
-      </div>
     </PagesLayout>
   )
 }
